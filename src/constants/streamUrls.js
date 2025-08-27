ec4b1f53e7385a5cd7d3f6ed244b995b9e221d78
iglesiacristovive <iglesia2025cristovive@gmail.com>
2025-08-26 21:50:03 -0300

    Actualización streamUrls.js

--- src/constants/streamUrls.js
+++ src/constants/streamUrls.js
@@ -1,6 +1,6 @@
 const streamUrls = {
-  youtube: 'https://youtu.be/q1SxbBtXLkw?si=VQbdcMRg3ivo-U9C',
+  youtube: 'https://www.youtube.com/embed/q1SxbBtXLkw',
   facebook: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2Fvideos%2F10153231379946729%2F&show_text=false',
-  tiktok: 'https://www.tiktok.com/embed/v2/7136018318721150250'
+  tiktok: 'https://www.tiktok.com/embed/v2/7535691192877567237'
 };
 export default streamUrls;
